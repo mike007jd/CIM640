5f33835db41ddc311c55da475e180fc7c1d721e2
mike007jd <31256541+mike007jd@users.noreply.github.com>
2017-10-31 23:24:22 -0400

    complete

--- myCharacterHere/bubup5sketch/sketchitFinished.js
+++ myCharacterHere/bubup5sketch/sketchitFinished.js
@@ -1,9 +1,8 @@
 var centerX = 0;
 var centerY = 0;
-
+var posY = 0;
 var bgColor;
 
-var hRedColor, hGreenColor, hBlueColor;
 
 var bgImage, bgImage2;
 
@@ -18,14 +17,25 @@ var hitZone1X = 400;
 var hitZone1Y = 200;
 
 var hitZone2X = 600;
-var hitZone2Y = 200;
+var hitZone2Y = 300;
 
 var hand;
+var hand2;
+
+var apple;
+var rock;
+
+var counter = 0;
+
+var directionY = false;
 
 function preload() {
     bgImage = loadImage("assets/daytime.jpg");
     bgImage2 = loadImage("assets/bedtime.jpg");
-    hand = loadImage("assets/ballons.png");
+    hand = loadImage("assets/hands.png");
+    hand2 = loadImage("assets/hand2.png");
+    apple = loadImage("assets/apple.png");
+    rock = loadImage("assets/Rock.png");
 }
 
 function setup() {
@@ -133,35 +143,144 @@ function draw() {
     var hitZoneDist2 = dist(hitZone2X,hitZone2Y,mouseX,mouseY);
     console.log("hitZoneDist: " + hitZoneDist);
 
-    if(hitZoneDist <= 5){
+    if(hitZoneDist <= 10 && currentBgImage == bgImage){
        console.log("We are totally in the zone!");
-        image(hand,0,0);
+        image(hand,25,125);
+       }
+     if(hitZoneDist <= 10 && currentBgImage == bgImage2){
+       console.log("We are totally in the zone!");
+        image(hand2,-50,-50);
        }
 
     stroke(0);
     strokeWeight(1);
     noFill();
-    ellipse(hitZoneX,hitZoneY, 10,10);
+    ellipse(hitZoneX,hitZoneY, 20,20);
     
-     if(hitZoneDist1 <= 5){
+     if(hitZoneDist1 <= 10){
        console.log("We are totally in the zone!");
-        image(hand,0,0);
-       }
+          frameRate(5);
 
-    stroke(0);
-    strokeWeight(1);
+    push();
+
+    scale(random(5.0));
+    //face
+    noStroke();
+    fill("#B97D36");
+    ellipse(centerX, centerY, 150, 145);
+    //nose
+    noStroke();
+    strokeWeight
+    fill("black");
+    ellipse(centerX,centerY+17,15,15);
     noFill();
-    ellipse(hitZone1X,hitZone1Y, 10,10);
+    stroke(0);
+    strokeWeight(2);
+    arc(centerX,centerY+20,60,60,QUARTER_PI,HALF_PI+QUARTER_PI);
+    arc(centerX,centerY+30,10,10,QUARTER_PI,HALF_PI+QUARTER_PI);
+    //eyse
     
-     if(hitZoneDist2 <= 5){
-       console.log("We are totally in the zone!");
-        image(hand,0,0);
+    stroke(0);
+    strokeWeight(2);
+    fill("white");
+    ellipse(centerX-7, centerY-30, 13, 30);
+    ellipse(centerX+7, centerY-30, 13, 30);
+    noFill();
+    stroke(0);
+    strokeWeight(2);
+    arc(centerX,centerY+10,50,50,PI+QUARTER_PI,PI+HALF_PI+QUARTER_PI);
+    noStroke();
+    fill("grey");
+    ellipse(centerX-7,centerY-40,10,10);
+    ellipse(centerX+7,centerY-40,10,10);
+    
+    //ear
+    
+     fill("#B97D36");
+    arc(centerX+45,centerY-40,50,100,PI+QUARTER_PI,PI+QUARTER_PI);
+    arc(centerX-45,centerY-40,50,100,HALF_PI+QUARTER_PI,PI+HALF_PI+QUARTER_PI);
+     stroke(0);
+    strokeWeight(2);
+    arc(centerX+45,centerY-50,25,50,PI+QUARTER_PI,TWO_PI);
+    arc(centerX-45,centerY-50,25,50,PI,PI+HALF_PI+QUARTER_PI);
+    
+    //inner face
+    
+     noFill();
+    stroke(0);
+    strokeWeight(2);
+    arc(centerX+45,centerY+15,50,50,HALF_PI,PI+HALF_PI);
+    arc(centerX-45,centerY+15,50,50,PI+HALF_PI,HALF_PI);
+    
+    //out face
+    
+    fill("#B97D36");
+    arc(centerX-35,centerY+10,100,100,HALF_PI,PI+QUARTER_PI);
+    arc(centerX+35,centerY+10,100,100,PI+HALF_PI+QUARTER_PI,HALF_PI);
+    //red face
+    
+    noStroke();
+    fill("red");
+    ellipse(centerX-50,centerY+17,25,25);
+    ellipse(centerX+50,centerY+17,25,25);
+    pop();
+
+
        }
 
     stroke(0);
     strokeWeight(1);
     noFill();
-    ellipse(hitZone2X,hitZone2Y, 10,10);
+    ellipse(hitZone1X,hitZone1Y, 20,20);
+    
+     if(hitZoneDist2 <= 10 && currentBgImage == bgImage){
+       console.log("We are totally in the zone!");
+        
+         frameRate(60);
+
+        image(apple,575, posY);
+        
+        if (directionY == false) {
+            posY++;
+        } else {
+            posY--;
+        }
+
+        if (posY >= height/2) {
+            directionY = true;
+        }
+
+        if (posY <= 0) {
+            directionY = false;
+        }
+       }
+    
+       if(hitZoneDist2 <= 10 && currentBgImage == bgImage2){
+       console.log("We are totally in the zone!");
+        
+         frameRate(60);
+
+        image(rock,575, posY);
+        
+        if (directionY == false) {
+            posY++;
+        } else {
+            posY--;
+        }
+
+        if (posY >= height/2) {
+            directionY = true;
+        }
+
+        if (posY <= 0) {
+            directionY = false;
+        }
+       }
+
+    stroke(0);
+    strokeWeight(1);
+    noFill();
+    ellipse(hitZone2X,hitZone2Y, 20,20);
 
 }
 
