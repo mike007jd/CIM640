5b64ffb497c9623faf2e1fe28622ee74c09da6f0
Haosheng Li <haoshengli@Haoshengs-MacBook-Pro.local>
2017-12-19 22:25:25 -0800

    reorganized

---
+++ Final/bearCrossingV1/empty-example/sketch.js
@@ -0,0 +1,30 @@
+var bubu;
+var bubuImage;
+var aligator;
+var aligatorImage;
+
+function setup() {
+    createCanvas(800, 600);
+    
+    bubuImage=loadImage("assets/bubu.png");
+    aligatorImage=loadImage("assets/aligator.png");
+    aligator= createSprite(300,200,40,40);
+    
+    bubu = createSprite( width / 2, height / 2,40,40);
+    bubu.shapeColor = color(255);
+    bubu.rotateToDirection = true;
+    bubu.maxSpeed = 2;
+
+    bubu.addImage("swim", bubuImage);
+    aligator.addImage("normal",aligatorImage);
+}
+
+function draw() {
+    background("green");
+    if (mouseIsPressed) {
+        bubu.attractionPoint(0.5, mouseX, mouseY);
+    }
+      textAlign(CENTER);
+  text("Click to move", width/2, 20);
+    drawSprites();
+}
